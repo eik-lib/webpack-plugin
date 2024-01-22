8ffdfa669d05808f0651c30447ad5a0208f6f542
William Killerud <william.killerud@schibsted.com>
2024-01-22 14:26:44 +0100

    fix: make string matcher more precise

--- src/parsers.cjs
+++ src/parsers.cjs
@@ -1,4 +1,4 @@
-const standardImport = (flags = 'sgm') => new RegExp('(import)\\s*(.+?)\\s*(from)\\s*[\'"](.+?)[\'"]', flags);
+const standardImport = (flags = 'sgm') => new RegExp('(import)\\s*(.+?)\\s*(from)\\s*[\'"]([\\w@\\-\\./]+?)[\'"]', flags);
 module.exports.standardImport = standardImport;
 
 const dynamicImport = (flags = 'gm') => new RegExp('(import)[(][\'"](.+?)[\'"][)]', flags);
