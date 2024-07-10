d08d4b56d4695878c27087aa83e3556904fb1bbc
William Killerud <william@killerud.com>
2024-07-10 15:00:23 +0200

    chore: set up eslint 9 and gitattributes (#135)
    The latter for cross-platform support

--- src/parsers.cjs
+++ src/parsers.cjs
@@ -1,8 +1,14 @@
-const standardImport = (flags = 'sgm') => new RegExp('(import)\\s*([\\w{},\\n\\s\\[\\]\\*\\.]+?)\\s*(from)\\s*[\'"]([\\w@\\-\\./]+?)[\'"]', flags);
+const standardImport = (flags = "sgm") =>
+  new RegExp(
+    "(import)\\s*([\\w{},\\n\\s\\[\\]\\*\\.]+?)\\s*(from)\\s*['\"]([\\w@\\-\\./]+?)['\"]",
+    flags,
+  );
 module.exports.standardImport = standardImport;
 
-const dynamicImport = (flags = 'gm') => new RegExp('(import)[(][\'"](.+?)[\'"][)]', flags);
+const dynamicImport = (flags = "gm") =>
+  new RegExp("(import)[(]['\"](.+?)['\"][)]", flags);
 module.exports.dynamicImport = dynamicImport;
 
-const sideEffectsImport = (flags = 'gm') => new RegExp('(import)\\s*[\'"](.+?)[\'"]', flags);
+const sideEffectsImport = (flags = "gm") =>
+  new RegExp("(import)\\s*['\"](.+?)['\"]", flags);
 module.exports.sideEffectsImport = sideEffectsImport;
