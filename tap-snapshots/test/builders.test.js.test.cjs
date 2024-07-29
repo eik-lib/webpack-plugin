d720e8008a1b38b4a50e79d8fee1f4bfd64cbaba
William Killerud <william.killerud@schibsted.com>
2024-07-29 14:42:40 +0200

    chore: update tap

---
+++ tap-snapshots/test/builders.test.js.test.cjs
@@ -0,0 +1,34 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/builders.test.js > TAP > .dynamicImport() - Replace module name > should replaced matched values with dictionary values 1`] = `
+
+        var a = import('./REPLACED-A.js')
+        let b = await import('module-b');
+        import('./REPLACED-C.js').then((module) => {
+            // Do something with the module.
+        });
+    
+`
+
+exports[`test/builders.test.js > TAP > .sideEffectsImport() - Replace module name > should replaced matched values with dictionary values 1`] = `
+
+        import './REPLACED-A.js';import 'module-b'
+        import './REPLACED-C.js';
+    
+`
+
+exports[`test/builders.test.js > TAP > .standardImport() - Replace module name > should replaced matched values with dictionary values 1`] = `
+
+        import { export1 } from './REPLACED-A.js';
+        import {export2} from 'module-b';
+        import { 
+            export3,
+            export4,
+        } from './REPLACED-C.js';
+        
+`
