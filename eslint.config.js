b2ce8df4aa411e54a4ddf54aaab05d0957939622
William Killerud <william.killerud@schibsted.com>
2024-08-08 16:47:38 +0200

    chore: use shared eslint config [skip ci]

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,3 @@
-import prettierConfig from "eslint-config-prettier";
-import prettierPlugin from "eslint-plugin-prettier/recommended";
-import globals from "globals";
-import js from "@eslint/js";
+import config from "@eik/eslint-config";
 
-export default [
-  js.configs.recommended,
-  prettierConfig,
-  prettierPlugin,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.browser,
-        global: true,
-      },
-    },
-  },
-  {
-    ignores: [
-      "tap-snapshots/*",
-      "node_modules/*",
-      "modules/*",
-      "utils/*",
-      "dist/*",
-      "tmp/*",
-    ],
-  },
-];
+export default config;
