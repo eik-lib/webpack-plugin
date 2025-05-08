289dec112afc64554b1be17f02cb3af11eb6f61e
William Killerud <william.killerud@schibsted.com>
2025-05-08 14:11:04 +0200

    fix: update eik/common for shared map loader
    BREAKING CHANGE: Drop support for Node 18.

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,3 @@
-import { request } from "undici";
-
 /**
  * Whether or not a string looks like a bare import.
  * @param {string} str
@@ -26,6 +24,11 @@ export const isBare = (str) => {
 export const isString = (str) => typeof str === "string";
 
 /**
+ * @typedef {object} ImportMap
+ * @property {Record<string, string>} imports
+ */
+
+/**
  * @param {ImportMap} map
  * @returns {Array<{ key: string; value: string; }>}
  */
@@ -41,35 +44,3 @@ export const validate = (map) =>
 
 		return { key, value };
 	});
-
-/**
- * @typedef {object} ImportMap
- * @property {Record<string, string>} imports
- */
-
-/**
- * @param {string[]} urls
- * @returns {Promise<ImportMap[]>}
- */
-export const fetchImportMaps = async (urls = []) => {
-	try {
-		const maps = urls.map(async (map) => {
-			const { statusCode, body } = await request(map, { maxRedirections: 2 });
-
-			if (statusCode === 404) {
-				throw new Error("Import map could not be found on server");
-			} else if (statusCode >= 400 && statusCode < 500) {
-				throw new Error("Server rejected client request");
-			} else if (statusCode >= 500) {
-				throw new Error("Server error");
-			}
-
-			return /** @type {Promise<ImportMap>} */ (body.json());
-		});
-		return await Promise.all(maps);
-	} catch (err) {
-		throw new Error(
-			`Unable to load import map file from server: ${err.message}`,
-		);
-	}
-};
